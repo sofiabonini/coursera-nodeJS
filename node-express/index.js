763dcac6b0c7717375039c05302c5f93987c82d7
Sofia Bonini <boninisofia14@gmail.com>
2021-04-06 17:51:37 +0200

    assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -3,14 +3,18 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,7 +22,6 @@ app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
 
 const server = http.createServer(app);
