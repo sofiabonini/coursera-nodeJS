e8d48fd0932b9aca35ab09b120313bb30652a58f
Sofia Bonini <sofia.bonini@stud.sbg.ac.at>
2021-05-13 11:16:51 +0200

    react using firebase BaaS

---
+++ conFusion-React-Firebase/src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyB84ZBKGQFLncVQ-AG5q6A8Bl21HfA7kJ8",
+  authDomain: "confusionserver-45356.firebaseapp.com",
+  projectId: "confusionserver-45356",
+  storageBucket: "confusionserver-45356.appspot.com",
+  messagingSenderId: "1013047989530",
+  appId: "1:1013047989530:web:9303486ab67d89401e08a3"
+};
